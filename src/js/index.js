e989eea062306c0beb7bb4d766ad465a566cb3ba
Heather Conley <hconley6695@gmail.com>
2016-10-12 12:35:23 -0400

    wrote two initial functions to get scenarios to load on page

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,24 @@
 // Javascript Entry Point
 import $ from 'jquery';
 import scenarios from './scenarios';
+
+function slide(scenario) {
+	var buttons = scenario.paths.map (path => {
+		return `<li><button> ${path.label} </button></li>`
+	});
+	return `<p> ${scenario.message}</p>
+			<ul>${buttons.join('')}</ul>`
+
+}
+
+function loadScene(scenario) {
+	var eachSlide = slide(scenario);
+	$('.app').html(eachSlide);
+
+}
+
+loadScene(scenarios.start_scenario);
+
+
+
+
