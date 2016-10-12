842b246e87d148ba5d0d16cd43013abbdb20b5f0
Heather Conley <hconley6695@gmail.com>
2016-10-12 16:07:52 -0400

    working on pulling data in for next scene

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,11 @@ import scenarios from './scenarios';
 
 function slide(scenario) {
 	var buttons = scenario.paths.map(path => {
-		return `<li><button> ${path.label} </button></li>`
+//THIS IS SENDING BACK OBJECT;
+		console.log(scenario);
+		//return `<li><button data-next="${path.scenarios.scenario}"> ${path.label} </button></li>`
+		//NEED HELP HERE!!!
+		return `<li><button data-next="${scenario}"> ${path.label} </button></li>`
 	});
 	return `<p> ${scenario.message}</p>
 			<ul>${buttons.join('')}</ul>`
@@ -12,19 +16,21 @@ function slide(scenario) {
 }
 
 function loadScene(scenario) {
+
 	var eachSlide = slide(scenario);
 	$('.app').html(eachSlide);
 
 }
 
-
+//console.log(scenarios.goTreasureHunting.paths[1].scenario);
 loadScene(scenarios.start_scenario);
 
 $('.app').on('click', 'button', event => {
-	//console.log('I clicked a button.');
-	var dataFromButton = $(event.target).data('eachSlide');
-	// console.log(scenario);
-	loadScene(dataFromButton);
+	var newScene = $(event.target).data('next');
+//THIS IS SENDING BACK VALUE OF KEY OF OBJECT, NOT OF OBJECT.  THIS I WHERE I NEED MY HELP.
+	console.log(newScene);
+
+	loadScene(newScene);
 
 });
 
