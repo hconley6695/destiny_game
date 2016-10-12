a906ad284e0f154040ee3c16a176b910fc4fee82
Heather Conley <hconley6695@gmail.com>
2016-10-12 16:41:15 -0400

    completed javascript, starting sass

--- src/js/index.js
+++ src/js/index.js
@@ -3,33 +3,26 @@ import $ from 'jquery';
 import scenarios from './scenarios';
 
 function slide(scenario) {
+	
 	var buttons = scenario.paths.map(path => {
-//THIS IS SENDING BACK OBJECT;
-		console.log(scenario);
-		//return `<li><button data-next="${path.scenarios.scenario}"> ${path.label} </button></li>`
-		//NEED HELP HERE!!!
-		return `<li><button data-next="${scenario}"> ${path.label} </button></li>`
+		return `<li><button data-next="${path.scenario}"> ${path.label} </button></li>`
 	});
-	return `<p> ${scenario.message}</p>
+	
+	return `<h3> ${scenario.message}</h3>
 			<ul>${buttons.join('')}</ul>`
 
 }
 
-function loadScene(scenario) {
-
+function loadScene(scenarioName) {
+	var scenario = scenarios[scenarioName];
 	var eachSlide = slide(scenario);
 	$('.app').html(eachSlide);
-
 }
 
-//console.log(scenarios.goTreasureHunting.paths[1].scenario);
-loadScene(scenarios.start_scenario);
+loadScene('start_scenario');
 
 $('.app').on('click', 'button', event => {
 	var newScene = $(event.target).data('next');
-//THIS IS SENDING BACK VALUE OF KEY OF OBJECT, NOT OF OBJECT.  THIS I WHERE I NEED MY HELP.
-	console.log(newScene);
-
 	loadScene(newScene);
 
 });
